40005b92293cfe5a78fbd7a8b07835a47dcb23a1
Johan Hovold <johan+linaro@kernel.org>
2023-02-02 16:54:33 +0100

    BACKPORT: rtc: pm8xxx: use unaligned le32 helpers
    Use the unaligned le32 helpers instead of open coding when accessing the
    time and alarm registers.
    
    Change-Id: Ic7f3f1f2f7e1d8b269b93b0329b6f0a9c0db48b9
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Reviewed-by: David Collins <quic_collinsd@quicinc.com>
    Link: https://lore.kernel.org/r/20230202155448.6715-8-johan+linaro@kernel.org
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    [ Tashar02: Backport and adapt to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- drivers/rtc/rtc-pm8xxx.c
+++ drivers/rtc/rtc-pm8xxx.c
@@ -13,6 +13,7 @@
 #include <linux/regmap.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
+#include <asm/unaligned.h>
 #ifdef CONFIG_RTC_AUTO_PWRON
 #include <linux/reboot.h>
 #include <linux/wakelock.h>
@@ -125,8 +126,7 @@ static int pm8xxx_rtc_read_rtc_data(struct pm8xxx_rtc *rtc_dd, unsigned long *rt
 			return rc;
 	}
 
-	*rtc_data = value[0] | (value[1] << 8) | (value[2] << 16) |
-			((unsigned long)value[3] << 24);
+	*rtc_data = get_unaligned_le32(value);
 
 	return 0;
 }
@@ -141,8 +141,7 @@ static int pm8xxx_rtc_read_alarm_data(struct pm8xxx_rtc *rtc_dd, unsigned long *
 	if (rc)
 		return rc;
 
-	*alarm_data = value[0] | (value[1] << 8) | (value[2] << 16) |
-			((unsigned long)value[3] << 24);
+	*alarm_data = get_unaligned_le32(value);
 
 	return 0;
 }
@@ -158,24 +157,20 @@ static int pm8xxx_rtc_read_alarm_data(struct pm8xxx_rtc *rtc_dd, unsigned long *
  */
 static int pm8xxx_rtc_set_time(struct device *dev, struct rtc_time *tm)
 {
-	int rc, i;
 	struct pm8xxx_rtc *rtc_dd = dev_get_drvdata(dev);
 	const struct pm8xxx_rtc_regs *regs = rtc_dd->regs;
 	u8 value[NUM_8_BIT_RTC_REGS];
 	bool alarm_enabled;
 	unsigned long secs;
+	int rc;
 
 	if (!rtc_dd->allow_set_time)
 		return -EACCES;
 
 	secs = rtc_tm_to_time64(tm);
+	put_unaligned_le32(secs, value);
 
 	dev_dbg(dev, "Seconds value to be written to RTC = %lu\n", secs);
-
-	for (i = 0; i < NUM_8_BIT_RTC_REGS; i++) {
-		value[i] = secs & 0xFF;
-		secs >>= 8;
-	}
 
 	rc = regmap_update_bits_check(rtc_dd->regmap, regs->alarm_ctrl,
 				      regs->alarm_en, 0, &alarm_enabled);
@@ -244,18 +239,14 @@ static int pm8xxx_rtc_read_time(struct device *dev, struct rtc_time *tm)
 
 static int pm8xxx_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alarm)
 {
-	int rc, i;
 	u8 value[NUM_8_BIT_RTC_REGS];
 	struct pm8xxx_rtc *rtc_dd = dev_get_drvdata(dev);
 	const struct pm8xxx_rtc_regs *regs = rtc_dd->regs;
 	unsigned long secs;
-
+	int rc;
 	secs = rtc_tm_to_time64(&alarm->time);
 
-	for (i = 0; i < NUM_8_BIT_RTC_REGS; i++) {
-		value[i] = secs & 0xFF;
-		secs >>= 8;
-	}
+	put_unaligned_le32(secs, value);
 
 	rc = regmap_update_bits(rtc_dd->regmap, regs->alarm_ctrl,
 				regs->alarm_en, 0);
