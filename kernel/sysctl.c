90e271e6e8277d74a95b35c05665706aa174b4dc
EmanuelCN <emanuelghub@gmail.com>
2024-10-18 12:12:30 +0300

    kernel/sched: Checkout to redbull-4.19-u
    Signed-off-by: skye-tachyon <sonicgames772@gmail.com>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -662,7 +662,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_thousand,
 	},
-#endif
 	{
 		.procname	= "sched_force_lb_enable",
 		.data		= &sysctl_sched_force_lb_enable,
@@ -672,6 +671,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname       = "sched_cstate_aware",
