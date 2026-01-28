1c64f34c8905320ce48b0673379b46a4f3fc4f0d
skye-pa1n <sonicgames772@gmail.com>
2026-01-28 12:23:55 -0300

    android: rom_notifier: provide more entries
    * you'll figure this out
    
    Change-Id: I5480166422028153b203453cbb1a78d5a2e32bf0
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- drivers/android/rom_notifier.c
+++ drivers/android/rom_notifier.c
@@ -3,6 +3,21 @@
  * Copyright (C) 2024 Samuel Pascua <pascua.samuel.14@gmail.com>.
  */
 
+/*
+ * aosp = aosp
+ * sunny = OneUI with the new usb notify sysfs naming:
+ 
+ static const char *const LOCK_STATE_NAMES[] = {
+    "SUNNY_WORK_MODE",     // 0: USB_NOTIFY_UNLOCK
+    "CLOUDY_WORK_MODE",    // 1: USB_NOTIFY_LOCK_USB_WORK
+    "RAINY_RESTRICT_MODE",     // 2: USB_NOTIFY_LOCK_USB_RESTRICT
+    "SKY_DEFAULT"        // 3: default
+};
+ 
+ * bpf = rom requires 5.10 uname (bpf)
+ * bpfl = rom requires 4.19 uname (bpf legacy)
+ */
+ 
 #include <linux/init.h>
 #include <linux/rom_notifier.h>
 #include <linux/string.h>
@@ -16,3 +31,33 @@ static int __init parse_aosp(char *str)
 	return 0;
 }
 __setup("android.is_aosp=", parse_aosp);
+
+bool is_sunny __read_mostly = false;
+static int __init parse_sunny(char *str)
+{
+	if (!strncmp(str, "1", 1))
+		is_sunny = true;
+
+	return 0;
+}
+__setup("android.is_sunny=", parse_sunny);
+
+bool is_bpf __read_mostly = false;
+static int __init parse_bpf(char *str)
+{
+	if (!strncmp(str, "1", 1))
+		is_bpf = true;
+
+	return 0;
+}
+__setup("android.is_bpf=", parse_bpf);
+
+bool is_bpfl __read_mostly = false;
+static int __init parse_bpfl(char *str)
+{
+	if (!strncmp(str, "1", 1))
+		is_bpfl = true;
+
+	return 0;
+}
+__setup("android.is_bpfl=", parse_bpfl);
--- include/linux/rom_notifier.h
+++ include/linux/rom_notifier.h
@@ -3,7 +3,25 @@
  * Copyright (C) 2024 Samuel Pascua <pascua.samuel.14@gmail.com>.
  */
 
+/*
+ * aosp = aosp
+ * sunny = OneUI with the new usb notify sysfs naming:
+ 
+ static const char *const LOCK_STATE_NAMES[] = {
+    "SUNNY_WORK_MODE",     // 0: USB_NOTIFY_UNLOCK
+    "CLOUDY_WORK_MODE",    // 1: USB_NOTIFY_LOCK_USB_WORK
+    "RAINY_RESTRICT_MODE",     // 2: USB_NOTIFY_LOCK_USB_RESTRICT
+    "SKY_DEFAULT"        // 3: default
+};
+ 
+ * bpf = rom requires 5.10 uname (bpf)
+ * bpfl = rom requires 4.19 uname (bpf legacy)
+ */
+
 #include <linux/cache.h>
 #include <linux/types.h>
 
 extern bool is_aosp __read_mostly;
+extern bool is_sunny __read_mostly;
+extern bool is_bpf __read_mostly;
+extern bool is_bpfl __read_mostly;
